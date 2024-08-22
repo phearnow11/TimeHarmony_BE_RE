35a5b0d751c3adaf4f2c6b7323b00e4aa8a35c5f
Phrimp <phien.nt@thdcybersecurity.xyz>
2024-08-22 10:26:13 +0700

    get revenue by day

---
+++ src/main/java/com/example/TimeHarmony/builder/repository/ChatRepository.java
@@ -0,0 +1,25 @@
+package com.example.TimeHarmony.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.TimeHarmony.entity.Chat;
+
+public interface ChatRepository extends JpaRepository<Chat, String> {
+    @Query("select c from Chat c where cur_member = :mid")
+    List<Chat> getMyChat(@Param("mid") UUID mid);
+
+    @Query("select c from Chat c where cur_member = :cur_id and with_member = :with_id")
+    List<Chat> getSpecificChat(@Param("cur_id") UUID cur_id, @Param("with_id") UUID with_id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete [dbo].[Chat] where cur_member = :cur_mid and with_member = :with_mid", nativeQuery = true)
+    void deleteChat(@Param("cur_mid") UUID cur_mid, @Param("with_mid") UUID with_mid);
+}
